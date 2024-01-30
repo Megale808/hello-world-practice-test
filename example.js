c16f2939a2bf4581c670f6d190848886edf0c383
Roger lee <rogerlee955@gmail.com>
2024-01-30 21:04:55 +0100

    git branch example

--- example.js
+++ example.js
@@ -3,3 +3,12 @@ console.log('hello change')
 
 console.log('test,test,test,test')
 console.log('Good job')
+console.log("hello");
+
+let sum = 0;
+
+for (let i = 0; i < 5; i++) {
+  sum += i;
+}
+
+console.log(sum);
